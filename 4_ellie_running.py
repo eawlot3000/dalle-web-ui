381ffcb5706427c267ecb3ca086302019d667732
kanagawasurfin <kanagawasurfing@outlook.com>
2023-11-18 21:39:09 -0500

    update

--- 4_ellie_running.py
+++ 4_ellie_running.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 import subprocess
-import time
 commands = [
     "cd ~/dalle-web-ui",
     #"source dalle_3.8/bin/activate",
+    "open -a "Google Chrome" "http://127.0.0.1:5000"",
     "python3 app.py",
 ]
-
 for cmd in commands:
     subprocess.run(cmd, shell=True, check=True)
-
-chrome_cmd = 'open -a "Google Chrome" "http://127.0.0.1:5000"'
-subprocess.run(chrome_cmd, shell=True)
