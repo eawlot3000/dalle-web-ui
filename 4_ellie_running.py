bc3663863ee1aa4995a648e749dd6a6915e8c13b
kanagawasurfin <kanagawasurfing@outlook.com>
2023-11-18 21:43:29 -0500

    updates

--- 4_ellie_running.py
+++ 4_ellie_running.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 import subprocess
 commands = [
-    "cd ~/dalle-web-ui",
-    #"source dalle_3.8/bin/activate",
     'open -a "Google Chrome" "http://127.0.0.1:5000"',
-    "python3 app.py",
+    "python3 ~/dalle-web-ui/app.py",
 ]
 for cmd in commands:
     subprocess.run(cmd, shell=True, check=True)
