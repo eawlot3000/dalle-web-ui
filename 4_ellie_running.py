7c9a1f85c7cb4eaebe24fc9444c6696685e41a94
grandell1234 <78573365+grandell1234@users.noreply.github.com>
2024-01-08 12:32:25 -0600

    Delete 4_ellie_running.py

--- 4_ellie_running.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import subprocess
-commands = [
-    'open -a "Google Chrome" "http://127.0.0.1:5000"',
-    "python3 ~/dalle-web-ui/app.py",
-]
-for cmd in commands:
-    subprocess.run(cmd, shell=True, check=True)
